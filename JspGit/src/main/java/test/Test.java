d2b9d50bb4648d9a49bc407ba4b1ecbcfbf3baa0
Kim Sojoong <Kim Sojoong@DESKTOP-FK0RMA1>
2023-01-27 11:25:23 +0900

    2. test_branch에서 Test.java를 작성합니다.

--- JspGit/src/main/java/test/Test.java
+++ JspGit/src/main/java/test/Test.java
@@ -14,4 +14,4 @@ public class Test extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at:").append(request.getContextPath()).append("Test");
 	}
-}
+} 
